9ae286efd0171bfef9ee21f05902a06658532fbc
rewe999 <zymek998@gmail.com>
2020-09-28 23:09:53 +0200

    Add scroll

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
     console.log('ready');
 
     const root = document.querySelector('#root');
-    const section = document.querySelectorAll('section');
-    
-    document.addEventListener('mousewheel',function(event) {
-        console.log(event.wheelDelta);
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+    document.addEventListener('mousewheel', function (event) {
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(() => {
+            isThrottled = false;
+        }, 1000);
+
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const isFirstSection = currentSectionIndex === 0;
+            if (isFirstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        console.log(currentSectionIndex);
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+        });
     })
 })
