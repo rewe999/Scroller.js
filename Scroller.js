838bc0446ec770af7341e04618680b4b54f93b65
rewe999 <zymek998@gmail.com>
2020-09-29 13:37:56 +0200

    Add nabigation to the Scroller feature

--- Scroller.js
+++ Scroller.js
@@ -9,6 +9,8 @@ class Scroller {
 
         console.log(this.currentSectionIndex);
         this.isThrottled = false;
+
+        this.drawNavigation();
     }
 
     isScrolledIntoView(el) {
@@ -49,9 +51,48 @@ class Scroller {
     }
 
     scrollToCurrentSection = () => {
+        this.selectActiveNavItem();
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: "smooth",
             block: "end",
         });
     }
+
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.setAttribute('class','scroller__navigation');
+        const list = document.createElement('ul');
+
+        this.sections.forEach((section,index) => {
+            const listItem = document.createElement('li');
+
+            listItem.addEventListener('click',() => {
+                this.currentSectionIndex = index;
+                this.scrollToCurrentSection();
+            });
+
+            list.appendChild(listItem);
+        });
+
+        this.navigationContainer.appendChild(list);
+
+        document.body.appendChild(this.navigationContainer);
+
+        this.selectActiveNavItem();
+    }
+
+    selectActiveNavItem = () => {
+        if(this.navigationContainer){
+            const navigationItems = this.navigationContainer.querySelectorAll('li');
+
+            navigationItems.forEach((item,index) => {
+                if(index === this.currentSectionIndex) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active');
+                }
+            })
+        }        
+    }
+
 }
